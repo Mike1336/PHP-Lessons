b1da82c97b8d483ca5d031c0628804b6db90c654
Mikhail <muzhikovm@bk.ru>
2019-03-26 19:14:26 +0300

    YII2 Framework: Second practical work

---
+++ yiiProject/migrations/m190204_174500_create_provider_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%provider}}`.
+ */
+class m190204_174500_create_provider_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%provider}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(),
+            'site' => $this->string(),
+            'rating' => $this->float(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%provider}}');
+    }
+}
---
+++ yiiProject/migrations/m190204_180240_create_car_table.php
@@ -0,0 +1,63 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%car}}`.
+ * Has foreign keys to the tables:
+ *
+ * - `{{%provider}}`
+ */
+class m190204_180240_create_car_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%car}}', [
+            'id' => $this->primaryKey(),
+            'provider_id' => $this->integer()->notNull(),
+            'name' => $this->string(),
+            'power' => $this->integer(),
+            'max_speed' => $this->integer(),
+        ]);
+
+        // creates index for column `provider_id`
+        $this->createIndex(
+            '{{%idx-car-provider_id}}',
+            '{{%car}}',
+            'provider_id'
+        );
+
+        // add foreign key for table `{{%provider}}`
+        $this->addForeignKey(
+            '{{%fk-car-provider_id}}',
+            '{{%car}}',
+            'provider_id',
+            '{{%provider}}',
+            'id',
+            'CASCADE'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        // drops foreign key for table `{{%provider}}`
+        $this->dropForeignKey(
+            '{{%fk-car-provider_id}}',
+            '{{%car}}'
+        );
+
+        // drops index for column `provider_id`
+        $this->dropIndex(
+            '{{%idx-car-provider_id}}',
+            '{{%car}}'
+        );
+
+        $this->dropTable('{{%car}}');
+    }
+}
